59c7a092ff6b1a8f0f1b1586987c95d7b3e43284
Stephanie <hsu031@ucr.edu>
2021-06-15 23:04:23 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# vue-weather
+### Introduction
+After learning from the resources, I Create a weather app using vue and fetching data from a Weather API.
+
+Get current weather from a restful weather api and set weather background.
+If the weather is sunny, then the background will be sunny picture, otherwise, the background will be cloud images.
+I did a little optimization to directly show the current date(so this is app is not for forcasting, this is current time weather board :
+let date = new Date();
+return date; 
+
+Key take away: how to use Vue Cli, and call API and Conditional rendering
+
+## Project setup
+```
+npm install
+```
+
+### Run the application
+
+```
+npm run serve
+```
+
+
+
+
+
+
---
+++ src/App.vue
@@ -0,0 +1,178 @@
+<template>
+<!--    Markup language -->
+  <div id="app" :class="typeof weather.main != 'undefined' && weather.weather[0].main === 'Clear' ? 'sunny' : 'cloudy'">
+
+    <main>
+      <div class="search-box">
+        <input
+          type="text"
+          class="search-bar"
+          placeholder="Type the city name..."
+          v-model="query"
+          @keypress="fetchWeather"
+        />
+      </div>
+
+      <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
+        <div class="location-box">
+          <div class="location">{{ weather.name }}, {{ weather.sys.country }}</div>
+          <div class="date">{{ dateBuilder() }}</div>
+        </div>
+
+        <div class="weather-box">
+          <div class="temp">{{ Math.round(weather.main.temp) }}°c</div>
+          <div class="weather">{{ weather.weather[0].main }}</div>
+        </div>
+      </div>
+    </main>
+  </div>
+</template>
+
+
+<!--JS part-->
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      api_key: 'd5cabe8e482c3710536129a38014b5a4',
+      url_base: 'https://api.openweathermap.org/data/2.5/',
+      query: '',
+      weather: {}
+    }
+  },
+  methods: {
+    fetchWeather (e) {
+      if (e.key === "Enter") {
+        fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
+          .then(res => {
+            return res.json();
+          }).then(this.setResults);
+      }
+    },
+    setResults (results) {
+      this.weather = results;
+    },
+    dateBuilder () {
+      // let d = new Date();
+      // let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+      // let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+      // let day = days[d.getDay()];
+      let date = new Date();
+      return date;
+    }
+  }
+}
+</script>
+
+
+<!--CSS style-->
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body {
+  font-family: 'montserrat', sans-serif;
+}
+#app {
+  background-image: url('./assets/cold-bg.jpeg');
+  background-size: 100% 100%;
+  background-repeat: no-repeat;
+  background-position: center;
+  transition: 0.4s;
+}
+#app.warm {
+  background-image: url('./assets/warm-bg.jpeg');
+}
+
+#app.raining {
+  background-image: url('./assets/raining.webp');
+}
+
+#app.snowing {
+  background-image: url('./assets/snowy.gif');
+}
+
+#app.sunny {
+  background-image: url('./assets/sunny.webp');
+}
+
+
+#app.cloudy {
+  background-image: url('./assets/cloudy.webp');
+}
+
+#app.fog {
+  background-image: url('./assets/foggy.gif');
+}
+
+
+main {
+  min-height: 100vh;
+  padding: 25px;
+  background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.75));
+}
+.search-box {
+  width: 100%;
+  margin-bottom: 30px;
+}
+.search-box .search-bar {
+  display: block;
+  width: 100%;
+  padding: 15px;
+
+  color: #313131;
+  font-size: 20px;
+  appearance: none;
+  border:none;
+  outline: none;
+  background: none;
+  box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.5);
+  border-radius: 0px 16px 0px 16px;
+  transition: 0.4s;
+}
+.search-box .search-bar:focus {
+  box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.75);
+  border-radius: 16px 0px 16px 0px;
+}
+.location-box .location {
+  color: #FFF;
+  font-size: 32px;
+  font-weight: 500;
+  text-align: center;
+  text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
+}
+.location-box .date {
+  color: #FFF;
+  font-size: 20px;
+  font-weight: 300;
+  font-style: italic;
+  text-align: center;
+}
+.weather-box {
+  text-align: center;
+}
+.weather-box .temp {
+  display: inline-block;
+  padding: 10px 25px;
+  color: #FFF;
+  font-size: 102px;
+  font-weight: 900;
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+  background-color:rgba(255, 255, 255, 0.25);
+  border-radius: 16px;
+  margin: 30px 0px;
+  box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+}
+.weather-box .weather {
+  color: #FFF;
+  font-size: 48px;
+  font-weight: 700;
+  font-style: italic;
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+}
+</style>
